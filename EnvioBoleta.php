13b55c2c09ec66a4abe38d3d7b4b8c616a7a19d1
mmedin41 <mmedina@losmegas.com.ve>
2021-03-08 08:36:49 -0300

    Se agrega cambios en los estados

--- EnvioBoleta.php
+++ EnvioBoleta.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * LibreDTE
+ * OpenEnvia
  * Copyright (C) SASCO SpA (https://sasco.cl)
  *
  * Este programa es software libre: usted puede redistribuirlo y/o
@@ -23,11 +23,12 @@
 
 namespace website\Dte;
 
-use \sasco\LibreDTE\Estado;
+use GuzzleHttp\Psr7\Response;
+use \sasco\OpenEnvia\Estado;
 
 /**
  * Clase que permite interacturar con el envío de boletas al SII mediante
- * las funcionalidades extras de LibreDTE.
+ * las funcionalidades extras de OpenEnvia.
  * Se provee como una clase aparte, porque es una funcionalidad que por defecto
  * viene desactivada.
  *
@@ -35,7 +36,38 @@ use \sasco\LibreDTE\Estado;
 class Utility_EnvioBoleta
 {
     private static $retry = 10; ///< Veces que se reintentará conectar a SII al usar el servicio web
-    private static $verificar_ssl = true; ///< Indica si se deberá verificar o no el certificado SSL del SII
+    private static $verificar_ssl = true; ///< Indica si se deberá verificar o no el certificado SSL del SII        
+    private static $PRODUCCION_1="https://api.sii.cl/recursos/v1";
+    private static $PRODUCCION_2="https://rahue.sii.cl/recursos/v1";
+    private static $CERTIFICACION_1="https://apicert.sii.cl/recursos/v1";
+    private static $CERTIFICACION_2="https://pangal.sii.cl/recursos/v1";
+    protected static $URL;
+    protected static $URLEnvio;
+
+    private static $revision_estados = [
+        'rechazados' => ['RSC', 'RCT', 'RCH', 'RFR'],
+        'no_final' => ['002', '004', '005', '007', '106', '107', '-11', '-8', 'VOF', 'PRD', 'SOK', 'CRT', 'REC'],
+    ]; ///< Posibles estados de revisión de envío al SII de los DTE
+
+    private static $rechazado=[
+        'RSC'=>'Rechazado por Schema',
+        'RCO'=>'Rechazado por consistencia',
+        'RCH'=>'Rechazado por errores en informacion',
+        'RFR' => 'Rechazado por error en firma',
+        'RCT' => 'Rechazado por Error en Caratula'
+
+    ];
+
+    private static function setserver(){
+        $certificacion = \sasco\OpenEnvia\Sii::getAmbiente();
+        if($certificacion==0){
+            self::$URL=self::$PRODUCCION_1;
+            self::$URLEnvio=self::$PRODUCCION_2;
+        }else{       
+            self::$URL=self::$CERTIFICACION_1;
+            self::$URLEnvio=self::$CERTIFICACION_2;
+        }
+    }
     /**
      * Método que envía un XML de EnvioBoleta al SII y entrega el Track ID del envío
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
@@ -44,12 +76,11 @@ class Utility_EnvioBoleta
      */
     public static function enviar($usuario, $empresa, $xml, $Firma, $gzip = false, $retry = null)
     {
-        $certificacion = \sasco\LibreDTE\Sii::getAmbiente();
+        self::setserver();
         ///OBTENCION DEL TOKEN
         $token=self::getToken($Firma);
         ////ENVIO DEL DOCUMENTO
-        $respuesta=self::enviarNew($usuario, $empresa, $xml, $token, $gzip, $retry);
-        
+        $respuesta=self::enviarNew($usuario, $empresa, $xml, $token, $gzip, $retry);        
         return $respuesta;
     }
 
@@ -61,7 +92,16 @@ class Utility_EnvioBoleta
      */
     public static function estado_normalizado($rut, $dv, $track_id, $Firma, $dte, $folio)
     {
+        self::setserver();
         //La consulta del estado aun se encuentra en desarrollo.
+        $empresa=$rut."-".$dv;
+        ///OBTENCION DEL TOKEN
+        $token=self::getToken($Firma);
+        $resp=self::consultaBoleta($rut, $dv, $track_id, $Firma, $dte, $folio,$token);
+        $r=array();
+        $r['estado']=$resp['codigo'];
+        $r['detalle']=$resp['descripcion'];
+        return $r;
     }
 
      /**
@@ -73,17 +113,16 @@ class Utility_EnvioBoleta
      * @version 2020-11-19
      */
     private static function getSeed()
-    {
-        
-        $ch = curl_init("https://apicert.sii.cl/recursos/v1/boleta.electronica.semilla");
+    {                
+        $ch = curl_init(self::$URL."/boleta.electronica.semilla");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept:application/xml') );
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $resp=curl_exec($ch);
         $respon=new \SimpleXMLElement($resp, LIBXML_COMPACT);
         if ($resp===false or (string)$respon->xpath('/SII:RESPUESTA/SII:RESP_HDR/ESTADO')[0]!=='00') {
-            \sasco\LibreDTE\Log::write(
-                \sasco\LibreDTE\Estado::AUTH_ERROR_SEMILLA,
-                \sasco\LibreDTE\Estado::get(\sasco\LibreDTE\Estado::AUTH_ERROR_SEMILLA)
+            \sasco\OpenEnvia\Log::write(
+                \sasco\OpenEnvia\Estado::AUTH_ERROR_SEMILLA,
+                \sasco\OpenEnvia\Estado::get(\sasco\OpenEnvia\Estado::AUTH_ERROR_SEMILLA)
             );
             return false;
         }
@@ -102,10 +141,10 @@ class Utility_EnvioBoleta
      */
     private static function getTokenRequest($seed, $Firma = [])
     {   
-        if (is_array($Firma))
-            $Firma = new \sasco\LibreDTE\FirmaElectronica($Firma);
+         if (is_array($Firma))
+            $Firma = new \sasco\OpenEnvia\FirmaElectronica($Firma);
         $seedSigned = $Firma->signXML(
-            (new \sasco\LibreDTE\XML())->generate([
+            (new \sasco\OpenEnvia\XML())->generate([
                 'getToken' => [
                     'item' => [
                         'Semilla' => $seed
@@ -114,9 +153,9 @@ class Utility_EnvioBoleta
             ])->saveXML()
         );
         if (!$seedSigned) {
-            \sasco\LibreDTE\Log::write(
-                \sasco\LibreDTE\Estado::AUTH_ERROR_FIRMA_SOLICITUD_TOKEN,
-                \sasco\LibreDTE\Estado::get(\sasco\LibreDTE\Estado::AUTH_ERROR_FIRMA_SOLICITUD_TOKEN)
+            \sasco\OpenEnvia\Log::write(
+                \sasco\OpenEnvia\Estado::AUTH_ERROR_FIRMA_SOLICITUD_TOKEN,
+                \sasco\OpenEnvia\Estado::get(\sasco\OpenEnvia\Estado::AUTH_ERROR_FIRMA_SOLICITUD_TOKEN)
             );
             return false;
         }
@@ -137,19 +176,18 @@ class Utility_EnvioBoleta
      * @version 2020-11-19
      */
     public static function getToken($Firma = [])
-    {   //var_dump("Entro en la función getToken");
+    {  
         if (!$Firma) return false;
         $semilla = self::getSeed();
         if (!$semilla) return false;
         $requestFirmado = self::getTokenRequest($semilla, $Firma);
         if (!$requestFirmado) return false;
-       // $xml = \sasco\LibreDTE\Sii::request('GetTokenFromSeed', 'getToken', $requestFirmado);
        $header = [
-        'User-Agent: Mozilla/4.0 (compatible; PROG 1.0; LibreDTE)',
+        'User-Agent: Mozilla/4.0 (compatible; PROG 1.0; OpenEnvia)',
         'Referer: https://libredte.cl',
         'Content-type: application/xml'
         ];
-        $ch = curl_init("https://apicert.sii.cl/recursos/v1/boleta.electronica.token");
+        $ch = curl_init(self::$URL."/boleta.electronica.token");
         curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER,  $header);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $requestFirmado);
@@ -157,9 +195,9 @@ class Utility_EnvioBoleta
         $resp=curl_exec($ch);
         $respon=new \SimpleXMLElement($resp, LIBXML_COMPACT);
         if ($resp===false or (string)$respon->xpath('/SII:RESPUESTA/SII:RESP_HDR/ESTADO')[0]!=='00') {
-            \sasco\LibreDTE\Log::write(
-                \sasco\LibreDTE\Estado::AUTH_ERROR_TOKEN,
-                \sasco\LibreDTE\Estado::get(\sasco\LibreDTE\Estado::AUTH_ERROR_TOKEN)
+            \sasco\OpenEnvia\Log::write(
+                \sasco\OpenEnvia\Estado::AUTH_ERROR_TOKEN,
+                \sasco\OpenEnvia\Estado::get(\sasco\OpenEnvia\Estado::AUTH_ERROR_TOKEN)
             );
            
             return false;
@@ -196,7 +234,7 @@ class Utility_EnvioBoleta
         if ($gzip) {
             $dte = gzencode($dte);
             if ($dte===false) {
-                \sasco\LibreDTE\Log::write(Estado::ENVIO_ERROR_GZIP, Estado::get(Estado::ENVIO_ERROR_GZIP));
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_GZIP, Estado::get(Estado::ENVIO_ERROR_GZIP));
                 return false;
             }
         }
@@ -219,12 +257,13 @@ class Utility_EnvioBoleta
         // crear sesión curl con sus opciones
         $curl = curl_init();
         $header = [
-            'User-Agent: Mozilla/4.0 (compatible; PROG 1.0; LibreDTE)',
+            'User-Agent: Mozilla/4.0 (compatible; PROG 1.0; OpenEnvia)',
             'Referer: https://libredte.cl',
             'Cookie: TOKEN='.$token,
         ];
-        //$url = 'https://'.self::$config['servidor'][self::getAmbiente()].'.sii.cl/cgi_dte/UPL/DTEUpload';
-        $url='https://pangal.sii.cl/recursos/v1/boleta.electronica.envio';
+        //$url='https://pangal.sii.cl/recursos/v1/boleta.electronica.envio';
+        $url=self::$URLEnvio.'/boleta.electronica.envio';
+        //throw new \Exception($url);
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
@@ -237,7 +276,7 @@ class Utility_EnvioBoleta
         if (!self::$verificar_ssl) {
             if (self::getAmbiente()==self::PRODUCCION) {
                 $msg = Estado::get(Estado::ENVIO_SSL_SIN_VERIFICAR);
-                \sasco\LibreDTE\Log::write(Estado::ENVIO_SSL_SIN_VERIFICAR, $msg, LOG_WARNING);
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_SSL_SIN_VERIFICAR, $msg, LOG_WARNING);
             }
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         }
@@ -253,10 +292,10 @@ class Utility_EnvioBoleta
         // verificar respuesta del envío y entregar error en caso que haya uno
         if (!$response or $response=='Error 500') {
             if (!$response) {
-                \sasco\LibreDTE\Log::write(Estado::ENVIO_ERROR_CURL, Estado::get(Estado::ENVIO_ERROR_CURL, curl_error($curl)));
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_CURL, Estado::get(Estado::ENVIO_ERROR_CURL, curl_error($curl)));
             }
             if ($response=='Error 500') {
-                \sasco\LibreDTE\Log::write(Estado::ENVIO_ERROR_500, Estado::get(Estado::ENVIO_ERROR_500));
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_500, Estado::get(Estado::ENVIO_ERROR_500));
             }
             return false;
         }
@@ -265,7 +304,118 @@ class Utility_EnvioBoleta
 
         $r=json_decode($response, true);
 
+        //$res=self::status_sended($rutSender,$dvSender,$r['track_id'],$r,$token,1);
+
         return $r;
     }
-}   
 
+    public static function consultaBoleta($rut, $dv, $track_id, $Firma, $dte, $folio,$token,$retry=1)
+    {
+        $curl = curl_init();
+        $header = [
+            'User-Agent: Mozilla/4.0 ( compatible; PROG 1.0; Windows NT)',
+            'Referer: https://utus.cl',
+            'Cookie: TOKEN='.$token,
+            'accept: application/json',
+        ];
+
+        
+        //throw new \Exception('  '.'$token '.$token);
+        $Emisor = new \website\Dte\Model_Contribuyente($rut);
+        $DteEmitido = new Model_DteEmitido($Emisor->rut, $dte, $folio, $Emisor->enCertificacion());
+        $date=date_create($DteEmitido->fecha);
+        $fecha_envio=date_format($date, 'd-m-Y');
+        $receptor_rut=explode("-",$DteEmitido->getReceptor()->getRUT());
+        $rut_receptor=str_replace('.','',$receptor_rut[0]);
+        $dv_receptor=$receptor_rut[1];
+        $url = self::$URL.'/boleta.electronica/'.$rut.'-'.$dv.'-'.$dte.'-'.$folio.'/estado?rut_receptor='.$rut_receptor.'&dv_receptor='.$dv_receptor.'&monto='.$DteEmitido->getTotal().'&fechaEmision='.$fecha_envio;
+       // $url = self::$URL.'/boleta.electronica.envio/'.$rut.'-'.$dv.'-'.$track_id;
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        // enviar al SII
+        for ($i=0; $i<$retry; $i++) {
+            $response = curl_exec($curl);
+            if ($response and $response!='Error 500') {
+                break;
+            }
+        }
+        // verificar respuesta del envío y entregar error en caso que haya uno
+        if (!$response or $response=='Error 500') {
+            if (!$response) {
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_CURL, Estado::get(Estado::ENVIO_ERROR_CURL, curl_error($curl)));
+            }
+            if ($response=='Error 500') {
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_500, Estado::get(Estado::ENVIO_ERROR_500));
+            }
+            return false;
+        }
+        // cerrar sesión curl
+        curl_close($curl);
+        //throw new \Exception('  '.'$token '.$token.' URL '. $url);
+
+        //echo $response;
+        //throw new \Exception($response);
+        //throw new \Exception(self::status_sended($rut, $dv,$track_id,$response,$token,$retry));
+        $respuesta=self::status_sended($rut, $dv,$track_id,$response,$token,$retry);
+        
+        
+        //if(in_array($respuesta['estado'],self::$rechazado)){
+        if(isset(self::$rechazado[$respuesta['estado']])){
+           // throw new \Exception('dentro de la condición '. $respuesta['estado']);
+            $resp['codigo']=$respuesta['estado'];
+            $resp['descripcion']=self::$rechazado[$respuesta['estado']];
+            return $resp;
+        }
+    
+        return json_decode($response, true);
+    }
+
+
+    public static function status_sended($rut,$dv,$track_id,$r,$token,$retry=1){
+        
+        $curl = curl_init();
+        $header = [
+            'User-Agent: Mozilla/4.0 ( compatible; PROG 1.0; Windows NT)',
+            'Referer: https://utus.cl',
+            'Cookie: TOKEN='.$token,
+            'accept: application/json',
+        ];
+
+        $url = self::$URL.'/boleta.electronica.envio/'.$rut.'-'.$dv.'-'.$track_id;
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        // enviar al SII
+        for ($i=0; $i<$retry; $i++) {
+            $response = curl_exec($curl);
+            if ($response and $response!='Error 500') {
+                break;
+            }
+        }
+        // verificar respuesta del envío y entregar error en caso que haya uno
+        if (!$response or $response=='Error 500') {
+            if (!$response) {
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_CURL, Estado::get(Estado::ENVIO_ERROR_CURL, curl_error($curl)));
+            }
+            if ($response=='Error 500') {
+                \sasco\OpenEnvia\Log::write(Estado::ENVIO_ERROR_500, Estado::get(Estado::ENVIO_ERROR_500));
+            }
+            return false;
+        }
+        // cerrar sesión curl
+        curl_close($curl);
+        $respuesta=json_decode($response, true);
+        $res['estado']=$respuesta['estado'];
+        $res['track_id']=$respuesta['trackid'];
+        $res['fecha_recepcion']=$respuesta['fecha_recepcion'];
+        
+        //throw new \Exception('respuesta: '.$res['track_id'].'-'.$res['estado']);
+
+        return $res;
+    }
+
+}   
